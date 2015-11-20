c60f3cbfac98120ae3b517165408c7f0da54897c
leokraken <joleocl@gmail.com>
2015-11-20 00:43:34 -0200

    Enable CORS localhost

---
+++ openshiftproject/src/main/java/com/sapo/security/RequestFilter.java
@@ -0,0 +1,25 @@
+package com.sapo.security;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@PreMatching
+public class RequestFilter implements ContainerRequestFilter {
+    
+    @Override
+    public void filter( ContainerRequestContext requestCtx ) throws IOException {
+        System.out.println( "Executing REST request filter" );
+ 
+        // When HttpMethod comes as OPTIONS, just acknowledge that it accepts...
+        if ( requestCtx.getRequest().getMethod().equals( "OPTIONS" ) ) {
+            // Just send a OK signal back to the browser
+            requestCtx.abortWith( Response.status( Response.Status.OK ).build() );
+        }
+    }
+}
---
+++ openshiftproject/src/main/java/com/sapo/security/ResponseFilter.java
@@ -0,0 +1,20 @@
+package com.sapo.security;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ResponseFilter implements ContainerResponseFilter {
+ 
+    public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
+        //log.info( "Executing REST response filter" );
+    	System.out.println("Executing REST response filter");
+        responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+    }
+}
